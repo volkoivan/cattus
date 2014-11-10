6b7b212f6dd5e47c9cb765cf12d16b35bced6fca
Insality <insality@gmail.com>
2014-11-10 22:16:07 +0600

    Added start project

---
+++ Cattus/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Player: MonoBehaviour {
+
+    public int Score;
+
+    private void Start() {
+        Score = 0;
+    }
+
+    private void Update() {
+        UpdateControl();
+    }
+
+    private void UpdateControl() {
+        if (Input.GetKey(KeyCode.RightArrow)) rigidbody2D.AddForce(new Vector2(20, 0));
+        if (Input.GetKey(KeyCode.LeftArrow)) rigidbody2D.AddForce(new Vector2(-20, 0));
+    }
+
+
+    // IsTrigger вызывает следующие методы: OnTriggerEnter, OnTriggerExit, OnTriggerStay
+    private void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("Collider action");
+        if (col.gameObject.tag == "Enemy")
+            Application.LoadLevel(Application.loadedLevel);
+        if (col.gameObject.tag == "Item"){
+            Score++;
+            col.gameObject.SetActive(false);
+        }
+    }
+}
