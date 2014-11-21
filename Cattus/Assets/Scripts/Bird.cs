710b5af7400e8a9798162e5339c312a1be73b5b4
YIvan <megavirus74@gmail.com>
2014-11-22 00:06:24 +0500

    Ещё что то

---
+++ Cattus/Assets/Scripts/Bird.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Bird : Enemy {
+
+    private int Direction=1;
+    public float AdVel = 0.001f;
+
+    // Use this for initialization
+    private void Start() {
+
+        rigidbody2D.velocity = new Vector2(HSpeed, 0);
+
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement() {
+        gameObject.transform.position+=new Vector3 (HSpeed,VSpeed,0);
+        VSpeed += AdVel * Direction;
+        if (VSpeed > 0.05f) Direction *= -1;
+        if (VSpeed < -0.05f) Direction *= -1;
+        if (gameObject.transform.position.x > Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x) gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x*(-1), gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+}
