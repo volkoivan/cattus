72b98529bbcb99ebfd8f047af9f66f9f6851f91a
YIvan <megavirus74@gmail.com>
2014-11-24 18:54:30 +0500

    Added Main Menu 18:54 24.11.2014

--- Cattus/Assets/Scripts/Bird.cs
+++ Cattus/Assets/Scripts/Bird.cs
@@ -19,9 +19,12 @@ public class Bird : Enemy {
 
     private void UpdateMovement() {
         gameObject.transform.position+=new Vector3 (HSpeed,VSpeed,0);
-        VSpeed += AdVel * Direction;
+        VSpeed += AdVel*Direction;
         if (VSpeed > 0.05f) Direction *= -1;
         if (VSpeed < -0.05f) Direction *= -1;
-        if (gameObject.transform.position.x > Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x) gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x*(-1), gameObject.transform.position.y, gameObject.transform.position.z);
+        if (gameObject.transform.position.x > Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x)
+            gameObject.transform.position =
+                new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x*(-1),
+                    gameObject.transform.position.y, gameObject.transform.position.z);
     }
 }
--- Cattus/Assets/Scripts/Placement.cs
+++ Cattus/Assets/Scripts/Placement.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 
 public class Placement : MonoBehaviour {
-    public float xOffset;
-    public float yOffset;
+    public float xOffset1, yOffset1;
+
     // Use this for initialization
-    private void Start() {
+    private void Start()
+    {
         gameObject.transform.position =
-            Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth / 100 * xOffset, Camera.main.pixelHeight / 100 * yOffset, 1));
+            Camera.main.ScreenToViewportPoint(new Vector3(Camera.main.pixelWidth / 100 * xOffset1,
+                Camera.main.pixelHeight / 100 * yOffset1, 1));
     }
 
-    // Update is called once per frame
-    private void Update() {
-    }
+
 }
