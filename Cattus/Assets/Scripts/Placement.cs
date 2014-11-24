680a3b316be1c0b79a1c72e2ebf69cc20f88fba4
YIvan <megavirus74@gmail.com>
2014-11-24 16:54:50 +0500

    Дубль два

---
+++ Cattus/Assets/Scripts/Placement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Placement : MonoBehaviour {
+    public float xOffset;
+    public float yOffset;
+    // Use this for initialization
+    private void Start() {
+        gameObject.transform.position =
+            Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth / 100 * xOffset, Camera.main.pixelHeight / 100 * yOffset, 1));
+    }
+
+    // Update is called once per frame
+    private void Update() {
+    }
+}
