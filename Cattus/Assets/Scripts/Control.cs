b3442e26627ecab011aa5a49f7625c688fb695d9
YIvan <megavirus74@gmail.com>
2014-11-24 18:55:23 +0500

    Added Main Menu

---
+++ Cattus/Assets/Scripts/Control.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Control : MonoBehaviour {
+    // Use this for initialization
+    private void Start() {
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if (Input.GetKey(KeyCode.RightArrow)) rigidbody2D.AddForce(new Vector2(5, 0));
+        if (Input.GetKey(KeyCode.LeftArrow)) rigidbody2D.AddForce(new Vector2(-5, 0));
+    }
+}
---
+++ Cattus/Assets/TextSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextSettings : MonoBehaviour {
+
+    public GUIStyle CustomFontStyle;
+    public GUIText GuiText;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
