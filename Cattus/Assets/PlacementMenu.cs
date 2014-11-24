679085dfb6aebae01c0f5dac8c609a1891bb0367
YIvan <megavirus74@gmail.com>
2014-11-24 19:21:22 +0500

    Switch added

---
+++ Cattus/Assets/PlacementMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlacementMenu : MonoBehaviour
+{
+    public float xOffset1, yOffset1;
+
+    // Use this for initialization
+    private void Start()
+    {
+        gameObject.transform.position =
+            Camera.main.ScreenToViewportPoint(new Vector3(Camera.main.pixelWidth / 100 * xOffset1,
+                Camera.main.pixelHeight / 100 * yOffset1, 1));
+    }
+
+
+}
---
+++ Cattus/Assets/SpacePressed.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class SpacePressed : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyUp(KeyCode.Space)) Application.LoadLevel("Test");
+	}
+}
